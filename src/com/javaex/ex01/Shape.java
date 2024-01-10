04ddaa16644eb07e83b89e2bbad4d1134ac965cc
ojiwon99 <dhwldnjs0420@naver.com>
2024-01-10 17:40:04 +0900

    2024-01-10 과제하기

---
+++ src/com/javaex/ex01/Shape.java
@@ -0,0 +1,15 @@
+package com.javaex.ex01;
+
+public abstract class Shape {
+	
+	//필드
+	protected String fillColor;
+	//생성자
+	public Shape(String fillColor){
+		this.fillColor=fillColor;
+	}
+	//일반메소드
+	public abstract void draw();
+}
+
+
---
+++ src/com/javaex/ex01/ShapeApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex01;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+
+		//Shape s = new Shape("빨강"); //Shape은 인스턴스화 할 수 없다.
+		Circle c1 = new Circle("녹색", 10);
+		Ractangle r1 = new Ractangle("빨강", 4, 4);
+		Shape sr1 = new Ractangle("빨강", 4, 4);
+		
+		//sr1을 그리는 메소드 호출
+		
+		sr1.draw();
+		
+		
+		
+		
+		
+		//sr1의 가로크기 출력
+		System.out.println("sr1의 가로는");
+		
+		
+	}
+}
+
+	
+	
